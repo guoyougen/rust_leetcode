aabf55669f4e500113d2a7003791de8a878f803d
guoyougen <guoyougen27@outlook.com>
2023-08-17 17:14:24 +0800

    now

---
+++ leetcode/1444.number-of-ways-of-cutting-a-pizza.rs
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=1444 lang=rust
+ * @lcpr version=21913
+ *
+ * [1444] 切披萨的方案数
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn ways(pizza: Vec<String>, k: i32) -> i32 {
+        println!["sdfsfs"];
+    }
+}
+
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// ["A..","AAA","..."]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ["A..","AA.","..."]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// ["A..","A..","..."]\n1\n
+// @lcpr case=end
+
+ */
+
---
+++ leetcode/5.longest-palindromic-substring.rs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=rust
+ * @lcpr version=21913
+ *
+ * [5] 最长回文子串
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {}
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "babad"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "cbbd"\n
+// @lcpr case=end
+
+ */
+
+
